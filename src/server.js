a571d8e0787fdee58562b01b4f90dd07989adb08
Aneeta George <aneetageorge@Aneetas-MacBook-Air.local>
2023-06-28 16:38:07 -0700

    Added mongodb connection to comments API and also reduced code duplication for mongodb connections

--- src/server.js
+++ src/server.js
@@ -1,20 +1,15 @@
-import express, { response } from 'express';
-import { MongoClient } from 'mongodb';
+import express from 'express';
+import {db, connect} from './db.js';
 
 const app = express();
 app.use(express.json()); //Parse json req body
 
 app.post('/hello', (req, res)=>{
-    // console.log(request.body);
     res.send(`Hello ${req.body.name}!`);
 });
 
 app.get('/api/articles/:name', async (req, res) => {
     const {name} = req.params;
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
-    await client.connect();
-
-    const db = client.db('my-blog-db');
     const article = await db.collection('articles').findOne({name});
 
     if (article)
@@ -29,11 +24,7 @@ app.get('/api/articles/:name', async (req, res) => {
 
 app.put('/api/articles/:name/upvote', async (req, res) => {
     const { name } = req.params;
-   
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
-    await client.connect();
 
-    const db = client.db('my-blog-db');
     await db.collection('articles').updateOne({name}, 
         {$inc: {'upvotes': 1}});
 
@@ -41,7 +32,6 @@ app.put('/api/articles/:name/upvote', async (req, res) => {
 
     if (article)
     {
-        article.upvotes += 1;
         res.send(`The article ${name} has ${article.upvotes} upvotes!`);
     }
     else
@@ -50,15 +40,18 @@ app.put('/api/articles/:name/upvote', async (req, res) => {
     }
 });
 
-app.post('/api/articles/:name/comments', (req, res) => {
+app.post('/api/articles/:name/comments', async (req, res) => {
     const {author, comment} = req.body;
     const { name } = req.params;
-    const article = articles.find(a => a.name == name);
+
+    await db.collection('articles').updateOne({name}, 
+        {$push: {comments: {author, comment}}});
+
+    const article = await db.collection('articles').findOne({name});
 
     if (article)
     {
-        article.comments.push({author, comment});
-        res.send(article.comments);
+        res.json(article.comments);
     }
     else
     {
@@ -66,6 +59,8 @@ app.post('/api/articles/:name/comments', (req, res) => {
     }
 });
 
-app.listen(8000, ()=>{
-    console.log('Server is listening on port 8000');
+connect( () => {
+    app.listen(8000, ()=>{
+        console.log('Server is listening on port 8000');
+});
 });
